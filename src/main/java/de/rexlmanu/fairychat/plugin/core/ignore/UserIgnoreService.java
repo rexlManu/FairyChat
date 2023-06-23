cb6fc8f8b9ac669b3a809a52e0723e2f0834940c
Emmanuel Lampe <mail@emmanuel-lampe.de>
2023-06-23 16:47:05 +0200

    refactor: use ImplementedBy annotation instead of binding for UserIgnoreService

--- src/main/java/de/rexlmanu/fairychat/plugin/core/ignore/UserIgnoreService.java
+++ src/main/java/de/rexlmanu/fairychat/plugin/core/ignore/UserIgnoreService.java
@@ -1,7 +1,9 @@
 package de.rexlmanu.fairychat.plugin.core.ignore;
 
+import com.google.inject.ImplementedBy;
 import java.util.UUID;
 
+@ImplementedBy(DefaultUserIgnoreService.class)
 public interface UserIgnoreService {
   boolean isIgnored(UUID userId, UUID targetId);
 
