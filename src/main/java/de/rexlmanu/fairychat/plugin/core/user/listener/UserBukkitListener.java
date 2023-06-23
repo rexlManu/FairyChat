ab4014ab80fd9f32b91553d2589139dafc55b4f0
Emmanuel Lampe <mail@emmanuel-lampe.de>
2023-06-23 12:06:16 +0200

    fix: handle players when plugin will be disabled

--- src/main/java/de/rexlmanu/fairychat/plugin/core/user/listener/UserBukkitListener.java
+++ src/main/java/de/rexlmanu/fairychat/plugin/core/user/listener/UserBukkitListener.java
@@ -4,10 +4,13 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import de.rexlmanu.fairychat.plugin.core.user.UserFactory;
 import de.rexlmanu.fairychat.plugin.core.user.UserService;
+import org.bukkit.Server;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -19,6 +22,7 @@ public class UserBukkitListener implements Listener {
   private final UserFactory userFactory;
   private final Plugin plugin;
   private final BukkitScheduler scheduler;
+  private final Server server;
 
   @Inject
   public UserBukkitListener(
@@ -26,11 +30,13 @@ public class UserBukkitListener implements Listener {
       UserFactory userFactory,
       JavaPlugin plugin,
       PluginManager pluginManager,
-      BukkitScheduler scheduler) {
+      BukkitScheduler scheduler,
+      Server server) {
     this.userService = userService;
     this.userFactory = userFactory;
     this.plugin = plugin;
     this.scheduler = scheduler;
+    this.server = server;
 
     pluginManager.registerEvents(this, plugin);
   }
@@ -48,5 +54,23 @@ public class UserBukkitListener implements Listener {
     this.userService
         .findUserById(event.getPlayer().getUniqueId())
         .ifPresent(this.userService::logout);
+  }
+
+  @EventHandler
+  public void handle(PluginEnableEvent event) {
+    if (event.getPlugin().getName().equals("FairyChat")) {
+      this.server
+          .getOnlinePlayers()
+          .forEach(player -> this.userService.login(this.userFactory.createFromPlayer(player)));
+    }
+  }
+
+  @EventHandler
+  public void handle(PluginDisableEvent event) {
+    if (event.getPlugin().getName().equals("FairyChat")) {
+      this.server
+          .getOnlinePlayers()
+          .forEach(player -> this.userService.logout(this.userFactory.createFromPlayer(player)));
+    }
   }
 }
