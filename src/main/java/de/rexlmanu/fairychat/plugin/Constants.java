cd5cf452b744cfa77eebdf74ffdc63b67f631517
Emmanuel Lampe <mail@emmanuel-lampe.de>
2023-06-23 16:44:53 +0200

    refactor: group channel constants and unify them with ending channel

--- src/main/java/de/rexlmanu/fairychat/plugin/Constants.java
+++ src/main/java/de/rexlmanu/fairychat/plugin/Constants.java
@@ -14,12 +14,12 @@ public class Constants {
   public static final String MESSAGING_CHANNEL = "fairychat:messaging";
   public static final String BROADCAST_CHANNEL = "fairychat:broadcast";
   public static final String PRIVATE_MESSAGING_CHANNEL = "fairychat:private_messaging";
+  public static final String USER_EVENTS_LOGIN_CHANNEL = "fairychat:user-events:login";
+  public static final String USER_EVENTS_LOGOUT_CHANNEL = "fairychat:user-events:logout";
+  public static final String USER_IGNORE_UPDATE_CHANNEL = "fairychat:user-ignore-update";
   public static final String USERS_KEY = "fairychat:users";
   public static final String USERNAMES_KEY = "fairychat:usernames";
-  public static final String USER_EVENTS_LOGIN = "fairychat:user-events:login";
-  public static final String USER_EVENTS_LOGOUT = "fairychat:user-events:logout";
   public static final String LAST_RECIPIENTS_KEY = "fairychat:last_recipients";
-  public static final String USER_IGNORE_UPDATE_CHANNEL = "fairychat:user-ignore-update";
 
   public static final int BSTATS_ID = 18786;
 
