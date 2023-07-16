e943b3a8005ec50cd6bebdc64ff84493aff12a68
Emmanuel Lampe <mail@emmanuel-lampe.de>
2023-07-16 04:36:29 +0200

    chore: change default values for custom messages

--- src/main/java/de/rexlmanu/fairychat/plugin/configuration/CustomMessages.java
+++ src/main/java/de/rexlmanu/fairychat/plugin/configuration/CustomMessages.java
@@ -14,8 +14,11 @@ public class CustomMessages {
   private boolean broadcastAdvancementMessages = true;
 
   @Comment("If you want to broadcast the messages to all servers via redis.")
-  private boolean broadcastJoinMessages = true;
+  private boolean broadcastJoinMessages = false;
 
-  @Comment("If you want to broadcast the messages to all servers via redis.")
-  private boolean broadcastQuitMessages = true;
+  @Comment({
+    "If you want to broadcast the messages to all servers via redis.",
+    "Be careful, with quick connect like velocity does, the quit event is triggered after the join event, so the message quit message will be sent after the join message."
+  })
+  private boolean broadcastQuitMessages = false;
 }
