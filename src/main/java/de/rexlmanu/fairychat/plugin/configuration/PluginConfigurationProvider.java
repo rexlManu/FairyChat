930a9a163ee760340d72e343f60b512573251d6e
Emmanuel Lampe <mail@emmanuel-lampe.de>
2023-07-19 19:41:22 +0200

    chore: display error in console with help text when configuration con't be loaded

--- src/main/java/de/rexlmanu/fairychat/plugin/configuration/PluginConfigurationProvider.java
+++ src/main/java/de/rexlmanu/fairychat/plugin/configuration/PluginConfigurationProvider.java
@@ -3,21 +3,35 @@ package de.rexlmanu.fairychat.plugin.configuration;
 import com.google.inject.Singleton;
 import de.exlll.configlib.YamlConfigurations;
 import java.nio.file.Path;
+import java.util.logging.Logger;
 import lombok.Getter;
 
 @Singleton
 public class PluginConfigurationProvider {
   @Getter private volatile PluginConfiguration configuration;
   private final Path dataFolder;
+  private final Logger logger;
 
-  public PluginConfigurationProvider(Path dataFolder) {
+  public PluginConfigurationProvider(Path dataFolder, Logger logger) {
     this.dataFolder = dataFolder;
+    this.logger = logger;
 
     this.loadConfig();
   }
 
   public void loadConfig() {
-    this.configuration =
-        YamlConfigurations.update(this.dataFolder.resolve("config.yml"), PluginConfiguration.class);
+    try {
+      this.configuration =
+          YamlConfigurations.update(
+              this.dataFolder.resolve("config.yml"), PluginConfiguration.class);
+    } catch (Exception e) {
+      this.logger.severe(
+          """
+The configuration file could not be loaded. This can happen if you have made a mistake in the configuration file.
+Also make sure that you have the latest version of the configuration file. If that is the case, rename the old configuration file and restart the server.
+""");
+
+      throw new RuntimeException("Could not load configuration file.", e);
+    }
   }
 }
