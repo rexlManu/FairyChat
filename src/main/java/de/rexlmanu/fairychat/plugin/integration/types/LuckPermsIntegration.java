404f618d2ba0e1b9f2de1439f8d8d4a49261f866
Emmanuel Lampe <mail@emmanuel-lampe.de>
2023-08-18 09:33:38 +0200

    feat: `fc_luckperms_prefix_by_group` placeholder
    This placeholder supports a group name as first argument and gives back the group prefix if the user has the group. This allows to display specific prefixes from groups!

--- src/main/java/de/rexlmanu/fairychat/plugin/integration/types/LuckPermsIntegration.java
+++ src/main/java/de/rexlmanu/fairychat/plugin/integration/types/LuckPermsIntegration.java
@@ -7,10 +7,14 @@ import de.rexlmanu.fairychat.plugin.integration.Integration;
 import de.rexlmanu.fairychat.plugin.integration.chat.PlaceholderSupport;
 import de.rexlmanu.fairychat.plugin.utility.LegacySupport;
 import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.Tag;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.cacheddata.CachedMetaData;
+import net.luckperms.api.model.group.Group;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.platform.PlayerAdapter;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
@@ -20,49 +24,73 @@ import org.jetbrains.annotations.Nullable;
 @Singleton
 @RequiredArgsConstructor(onConstructor = @__(@Inject))
 public class LuckPermsIntegration implements Integration, PlaceholderSupport {
-    private final PluginManager pluginManager;
-    private final ServicesManager servicesManager;
-    private @Nullable RegisteredServiceProvider<LuckPerms> luckPermsService;
+  private final PluginManager pluginManager;
+  private final ServicesManager servicesManager;
+  private @Nullable RegisteredServiceProvider<LuckPerms> luckPermsService;
 
-    @Override
-    public boolean available() {
-        return this.pluginManager.getPlugin(Constants.LUCKPERMS_NAME) != null;
-    }
+  @Override
+  public boolean available() {
+    return this.pluginManager.getPlugin(Constants.LUCKPERMS_NAME) != null;
+  }
 
-    @Override
-    public void enable() {
-        this.luckPermsService = servicesManager.getRegistration(LuckPerms.class);
-    }
+  @Override
+  public void enable() {
+    this.luckPermsService = servicesManager.getRegistration(LuckPerms.class);
+  }
 
-    @Override
-    public TagResolver resolvePlayerPlaceholder(Player player) {
-        CachedMetaData metaData = this.luckPermsService.getProvider().getPlayerAdapter(Player.class)
-                .getMetaData(player);
+  @Override
+  public TagResolver resolvePlayerPlaceholder(Player player) {
+    PlayerAdapter<Player> playerAdapter =
+        this.luckPermsService.getProvider().getPlayerAdapter(Player.class);
+    CachedMetaData metaData = playerAdapter.getMetaData(player);
 
-        return TagResolver.resolver(
-                TagResolver.resolver(
-                        "fc_luckperms_prefix",
-                        Tag.selfClosingInserting(LegacySupport.parsePossibleLegacy(metaData.getPrefix()))),
-                TagResolver.resolver(
-                        "fc_luckperms_suffix",
-                        Tag.selfClosingInserting(LegacySupport.parsePossibleLegacy(metaData.getSuffix()))),
-                TagResolver.resolver(
-                        "fc_luckperms_prefixes",
-                        Tag.selfClosingInserting(
-                                LegacySupport.parsePossibleLegacy(
-                                        String.join("", metaData.getPrefixes().values())))),
-                TagResolver.resolver(
-                        "fc_luckperms_suffixes",
-                        Tag.selfClosingInserting(
-                                LegacySupport.parsePossibleLegacy(
-                                        String.join("", metaData.getSuffixes().values())))),
-                TagResolver.resolver(
-                        "fc_luckperms_username_color",
-                        Tag.inserting(
-                                LegacySupport.parsePossibleLegacy(metaData.getMetaValue("username-color")))),
-                TagResolver.resolver(
-                        "fc_luckperms_message_color",
-                        Tag.inserting(
-                                LegacySupport.parsePossibleLegacy(metaData.getMetaValue("message-color")))));
-    }
+    return TagResolver.resolver(
+        TagResolver.resolver(
+            "fc_luckperms_prefix",
+            Tag.selfClosingInserting(LegacySupport.parsePossibleLegacy(metaData.getPrefix()))),
+        TagResolver.resolver(
+            "fc_luckperms_suffix",
+            Tag.selfClosingInserting(LegacySupport.parsePossibleLegacy(metaData.getSuffix()))),
+        TagResolver.resolver(
+            "fc_luckperms_prefixes",
+            Tag.selfClosingInserting(
+                LegacySupport.parsePossibleLegacy(
+                    String.join("", metaData.getPrefixes().values())))),
+        TagResolver.resolver(
+            "fc_luckperms_suffixes",
+            Tag.selfClosingInserting(
+                LegacySupport.parsePossibleLegacy(
+                    String.join("", metaData.getSuffixes().values())))),
+        TagResolver.resolver(
+            "fc_luckperms_username_color",
+            Tag.inserting(
+                LegacySupport.parsePossibleLegacy(metaData.getMetaValue("username-color")))),
+        TagResolver.resolver(
+            "fc_luckperms_message_color",
+            Tag.inserting(
+                LegacySupport.parsePossibleLegacy(metaData.getMetaValue("message-color")))),
+        TagResolver.resolver(
+            "fc_luckperms_prefix_by_group",
+            (argumentQueue, context) -> {
+              String groupName =
+                  argumentQueue
+                      .popOr("Tag `fc_luckperms_prefix_by_group` requires a group name")
+                      .value();
+
+              Group group =
+                  this.luckPermsService.getProvider().getGroupManager().getGroup(groupName);
+              if (group == null) {
+                return Tag.selfClosingInserting(Component.empty());
+              }
+              User user = playerAdapter.getUser(player);
+
+              if (!user.getInheritedGroups(user.getQueryOptions()).contains(group)) {
+                return Tag.selfClosingInserting(Component.empty());
+              }
+
+              return Tag.selfClosingInserting(
+                  LegacySupport.parsePossibleLegacy(
+                      group.getCachedData().getMetaData().getPrefix()));
+            }));
+  }
 }
