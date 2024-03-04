8bc3e636c4cd3a79c4810b2da73f212b7aac9bb6
Emmanuel Lampe <mail@emmanuel-lampe.de>
2024-03-04 20:29:41 +0100

    fix: move configlib to maven central

--- src/main/java/de/rexlmanu/fairychat/plugin/FairyChatLoader.java
+++ src/main/java/de/rexlmanu/fairychat/plugin/FairyChatLoader.java
@@ -21,7 +21,7 @@ public class FairyChatLoader implements PluginLoader {
         new Dependency(new DefaultArtifact("cloud.commandframework:cloud-paper:1.8.3"), null));
     resolver.addDependency(
         new Dependency(
-            new DefaultArtifact("com.github.Exlll.ConfigLib:configlib-paper:v4.2.0"), null));
+            new DefaultArtifact("de.exlll:configlib-yaml:4.5.0"), null));
     resolver.addDependency(
         new Dependency(new DefaultArtifact("com.google.inject:guice:7.0.0"), null));
     resolver.addDependency(new Dependency(new DefaultArtifact("com.zaxxer:HikariCP:5.0.1"), null));
