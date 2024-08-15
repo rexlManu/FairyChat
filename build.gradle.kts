962d157a92a0c921ce3d8bb2528b15c68e42382a
Emmanuel Lampe <mail@emmanuel-lampe.de>
2024-08-15 08:55:28 +0200

    build: add newer versions as supported

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,7 +95,7 @@ tasks {
 
 tasks.getByName("modrinth").dependsOn(tasks.modrinthSyncBody)
 
-val versions = listOf("1.19.4", "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4");
+val versions = listOf("1.19.4", "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6", "1.21", "1.21.1");
 
 modrinth {
     token.set(System.getenv("MODRINTH_TOKEN"))
